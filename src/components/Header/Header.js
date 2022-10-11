1560c52d734f771eda02ef5a83c3aa1d3128be4e
sabikonnahar.sr <sabikonnahar.sr@gmail.com>
2022-10-11 14:53:32 +0600

    added header img and text

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,19 +2,24 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import "./Header.css";
 // import "../../images/background2.png";
+import '../images/background2.png'
 
 const Header = () => {
   return (
     <div>
       <nav className="nav-link">
-         <h1 className="text-4xl font-extrabold  text-slate-600">Retrival Sages</h1>
-          <NavLink className={({isActive}) => isActive ? 'active': undefined} to="/topics">Topics</NavLink>
-          <NavLink className={({isActive}) => isActive ? 'active': undefined} to="/statistics">Statistics</NavLink>
-          <NavLink to="/blog">Blog</NavLink>     
+             <div>
+             <h1 className="text-4xl font-extrabold  text-cyan-400">Retrival Sages</h1>
+             </div>
+           <div>
+            <NavLink className={({isActive}) => isActive ? 'active': undefined} to="/topics">Topics</NavLink>
+            <NavLink className={({isActive}) => isActive ? 'active': undefined} to="/statistics">Statistics</NavLink>
+            <NavLink to="/blog">Blog</NavLink> 
+           </div>    
       </nav>
-      <div className="header">
-        {/* <img src="images/background2.png" alt="" /> */}
-        <h2>“Any fool can write code that a computer can understand. ...</h2>
+      <div className="header-container">
+        <img src="images/background2.png" alt="" />
+        <h2 className="text-2xl text-slate-600 font-bold pt-7 ">“Any fool can write code that a computer can understand....</h2>
       </div>
     </div>
   );
