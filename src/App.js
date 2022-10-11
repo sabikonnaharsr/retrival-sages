c3fb9c0daecab5a234aea05cced31810ea25ce2d
sabikonnahar.sr <sabikonnahar.sr@gmail.com>
2022-10-11 13:23:24 +0600

    path added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Header from "./components/Header/Header";
 import Blog from "./components/Nav/Blog/Blog";
 import Statistics from "./components/Nav/Statistics/Statistics";
 import Topics from "./components/Nav/Topics/Topics";
+import ErrorPage from "./components/ErrorPage/ErrorPage";
+import Chart from "./components/Chart/Chart";
 
 function App() {
   const router = createBrowserRouter([
@@ -12,6 +14,8 @@ function App() {
     { path: "/topics" },
     { path: "/statistics", element: <Statistics></Statistics> },
     { path: "/blog", element: <Blog></Blog> },
+    { path: "/chart", element: <Chart></Chart> },
+    { path: "/errorPage", element: <ErrorPage></ErrorPage> },
   ]);
   return (
     <div className="App">
--- src/components/ErrorPage/ErrorPage.js
+++ src/components/ErrorPage/ErrorPage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ErrorPage = () => {
     return (
         <div>
-            <h1>Sorry Developer This Route Is Not Found!!</h1>
+            <h1>Sorry Developer This Route Is Not Found!! 404</h1>
         </div>
     );
 };
