8d6c69ef8c811bc97ef91aa3c61c4faca41357b4
sabikonnahar.sr <sabikonnahar.sr@gmail.com>
2022-10-11 13:19:45 +0600

    added chart page

---
+++ src/components/Chart/Chart.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Chart.css';
+
+const Chart = () => {
+    return (
+        <div>
+            <h1>i AM from chart</h1>
+        </div>
+    );
+};
+
+export default Chart;
---
+++ src/components/ErrorPage/ErrorPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ErrorPage = () => {
+    return (
+        <div>
+            <h1>Sorry Developer This Route Is Not Found!!</h1>
+        </div>
+    );
+};
+
+export default ErrorPage;
