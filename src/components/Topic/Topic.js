b9cb40ab8c2d4dd00b47d64b31e30b93e93578a0
sabikonnahar.sr <sabikonnahar.sr@gmail.com>
2022-10-12 17:23:03 +0600

    added height in card img

--- src/components/Topic/Topic.js
+++ src/components/Topic/Topic.js
@@ -13,9 +13,9 @@ const Topic = ({ topic }) => {
       <img
         src={logo}
         alt=""
-        className="object-cover object-center rounded-md w-full h-72 bg-gray-400"
+        className="object-cover object-center rounded-md w-full h-64 bg-gray-400"
       />
-      <div className="flex justify-between items-center py-4 px-3">
+      <div className="flex justify-between items-center py-7 px-3">
         <h1 className="font-bold text-2xl text-cyan-500">{name}</h1>
         <h3 className="text-xl font-bold">Quiz Total: {total}</h3>
         <button
