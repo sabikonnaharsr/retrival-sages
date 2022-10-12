706f0e8a9552eeffe71425d7d2e4367cea15ba7b
sabikonnahar.sr <sabikonnahar.sr@gmail.com>
2022-10-12 20:10:47 +0600

    added margin on footer

--- src/components/Nav/Blog/Blog.js
+++ src/components/Nav/Blog/Blog.js
@@ -52,7 +52,7 @@ const Blog = () => {
 
       {/* footer start */}
       <div>
-      <footer className="px-4 divide-y shadow-2xl dark:text-gray-900 font-semibold">
+      <footer className="px-4 mt-10 divide-y shadow-2xl dark:text-gray-900 font-semibold">
 	<div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
 		<div className="lg:w-1/3">
 			<a rel="noopener noreferrer" href="#" className="flex justify-center space-x-3 lg:justify-start">
