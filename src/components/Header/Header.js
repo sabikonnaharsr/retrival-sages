c3c844ef3535e545f4fb5dbd55804245b766ef8f
sabikonnahar.sr <sabikonnahar.sr@gmail.com>
2022-10-12 19:08:52 +0600

    added shadow

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -60,8 +60,8 @@ const Header = () => {
 
 {/* footer  */}
     <div>
-    <footer className="px-4 divide-y shadow shadow-xl dark:text-gray-800">
-	<div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
+    <footer className="px-4 divide-y shadow-xxl dark:text-gray-900">
+	<div className="container flex flex-col shadow-2xl justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
 		<div className="lg:w-1/3">
 			<a rel="noopener noreferrer" href="#" className="flex justify-center space-x-3 lg:justify-start">
 				<div className="flex items-center justify-center w-12 h-12 rounded-full dark:bg-violet-400">
@@ -74,7 +74,7 @@ const Header = () => {
 		</div>
 		<div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
 			<div className="space-y-3">
-				<h3 className="tracking-wide uppercase dark:text-gray-800">Product</h3>
+				<h3 className="tracking-wide uppercase dark:text-gray-900">Product</h3>
 				<ul className="space-y-1">
 					<li>
 						<a rel="noopener noreferrer" href="#">Features</a>
@@ -91,7 +91,7 @@ const Header = () => {
 				</ul>
 			</div>
 			<div className="space-y-3">
-				<h3 className="tracking-wide uppercase dark:text-gray-800">Topics</h3>
+				<h3 className="tracking-wide uppercase dark:text-gray-900">Topics</h3>
 				<ul className="space-y-1">
 					<li>
 						<a rel="noopener noreferrer" href="#">Future Native React</a>
@@ -105,7 +105,7 @@ const Header = () => {
 				</ul>
 			</div>
 			<div className="space-y-3">
-				<h3 className="uppercase dark:text-gray-800">Developers</h3>
+				<h3 className="uppercase dark:text-gray-900">Developers</h3>
 				<ul className="space-y-1">
 					<li>
 						<a rel="noopener noreferrer" href="#">Public API</a>
@@ -119,7 +119,7 @@ const Header = () => {
 				</ul>
 			</div>
 			<div className="space-y-3">
-				<div className="uppercase dark:text-gray-800">Social media</div>
+				<div className="uppercase dark:text-gray-900">Social media</div>
 				<div className="flex items-center justify-center space-x-3">
 					<a rel="noopener noreferrer" href="#" title="Facebook" className="flex items-center p-1">
 						<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
@@ -140,7 +140,7 @@ const Header = () => {
 			</div>
 		</div>
 	</div>
-	<div className="py-6 text-sm text-center dark:text-gray-400">© 2022 All rights reserved, sabikonnahar.sr@gmailcom</div>
+	<div className="py-6 text-sm text-center dark:text-gray-900 font-medium">© 2022 All rights reserved, sabikonnahar.sr@gmailcom</div>
 </footer>
     </div>
 
