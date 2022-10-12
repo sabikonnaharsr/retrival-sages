00d0b76c6fcfb5abdae2f0f379dff79356efa0af
sabikonnahar.sr <sabikonnahar.sr@gmail.com>
2022-10-12 18:57:29 +0600

    added shadow in footer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -60,7 +60,7 @@ const Header = () => {
 
 {/* footer  */}
     <div>
-    <footer className="px-4 divide-y shadow-lg bg-slate-200 dark:text-gray-800">
+    <footer className="px-4 divide-y shadow-slate-50 shadow-xl  dark:text-gray-800">
 	<div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
 		<div className="lg:w-1/3">
 			<a rel="noopener noreferrer" href="#" className="flex justify-center space-x-3 lg:justify-start">
