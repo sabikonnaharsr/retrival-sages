12030349d86a5c6a67b876d76a2b35a36a41e5dc
sabikonnahar.sr <sabikonnahar.sr@gmail.com>
2022-10-12 16:16:13 +0600

    added blog

--- src/components/Nav/Blog/Blog.js
+++ src/components/Nav/Blog/Blog.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Blog = () => {
   return (
-    <section className="dark:bg-gray-800 mt-28 dark:text-gray-100 w-80">
+    <section className="dark:bg-gray-800 mt-28 dark:text-gray-100 w-auto">
       <div className="container flex flex-col justify-center px-4 py-8 mx-auto md:p-8">
         <h2 className="text-2xl font-semibold sm:text-4xl">
           Frequently Asked Questions
