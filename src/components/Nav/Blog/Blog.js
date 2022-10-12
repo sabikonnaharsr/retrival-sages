eb88caa412c30bde657e1bb45f639217222e30c8
sabikonnahar.sr <sabikonnahar.sr@gmail.com>
2022-10-12 20:07:48 +0600

    footer replace in blog

--- src/components/Nav/Blog/Blog.js
+++ src/components/Nav/Blog/Blog.js
@@ -51,23 +51,22 @@ const Blog = () => {
       </section>
 
       {/* footer start */}
-        <div>
-        <div>
-    <footer className="px-4 mt-10 divide-y shadow-2xl dark:text-gray-50  dark:bg-gray-800 font-semibold">
+      <div>
+      <footer className="px-4 divide-y shadow-2xl dark:text-gray-900 font-semibold">
 	<div className="container flex flex-col justify-between py-10 mx-auto space-y-8 lg:flex-row lg:space-y-0">
 		<div className="lg:w-1/3">
 			<a rel="noopener noreferrer" href="#" className="flex justify-center space-x-3 lg:justify-start">
-				{/* <div className="flex items-center justify-center w-12 h-12 rounded-full dark:bg-violet-400">
+				<div className="flex items-center justify-center w-12 h-12 rounded-full dark:bg-violet-400">
 					<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" fill="currentColor" className="flex-shrink-0 w-5 h-5 rounded-full dark:text-gray-900">
 						<path d="M18.266 26.068l7.839-7.854 4.469 4.479c1.859 1.859 1.859 4.875 0 6.734l-1.104 1.104c-1.859 1.865-4.875 1.865-6.734 0zM30.563 2.531l-1.109-1.104c-1.859-1.859-4.875-1.859-6.734 0l-6.719 6.734-6.734-6.734c-1.859-1.859-4.875-1.859-6.734 0l-1.104 1.104c-1.859 1.859-1.859 4.875 0 6.734l6.734 6.734-6.734 6.734c-1.859 1.859-1.859 4.875 0 6.734l1.104 1.104c1.859 1.859 4.875 1.859 6.734 0l21.307-21.307c1.859-1.859 1.859-4.875 0-6.734z"></path>
 					</svg>
-				</div> */}
+				</div>
 				<span className="self-center text-2xl font-semibold">Brand name</span>
 			</a>
 		</div>
 		<div className="grid grid-cols-2 text-sm gap-x-3 gap-y-8 lg:w-2/3 sm:grid-cols-4">
 			<div className="space-y-3">
-				<h3 className="tracking-wide uppercase dark:text-gray-50">Product</h3>
+				<h3 className="tracking-wide uppercase dark:text-gray-900">Product</h3>
 				<ul className="space-y-1">
 					<li>
 						<a rel="noopener noreferrer" href="#">Features</a>
@@ -84,7 +83,7 @@ const Blog = () => {
 				</ul>
 			</div>
 			<div className="space-y-3">
-				<h3 className="tracking-wide uppercase dark:text-gray-50">Topics</h3>
+				<h3 className="tracking-wide uppercase dark:text-gray-900">Topics</h3>
 				<ul className="space-y-1">
 					<li>
 						<a rel="noopener noreferrer" href="#">Future Native React</a>
@@ -98,7 +97,7 @@ const Blog = () => {
 				</ul>
 			</div>
 			<div className="space-y-3">
-				<h3 className="uppercase dark:text-gray-50">Developers</h3>
+				<h3 className="uppercase dark:text-gray-900">Developers</h3>
 				<ul className="space-y-1">
 					<li>
 						<a rel="noopener noreferrer" href="#">Public API</a>
@@ -112,7 +111,7 @@ const Blog = () => {
 				</ul>
 			</div>
 			<div className="space-y-3">
-				<div className="uppercase dark:text-gray-50">Social media</div>
+				<div className="uppercase dark:text-gray-900">Social media</div>
 				<div className="flex items-center justify-center space-x-3">
 					<a rel="noopener noreferrer" href="#" title="Facebook" className="flex items-center p-1">
 						<svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 32 32" className="w-5 h-5 fill-current">
@@ -133,11 +132,10 @@ const Blog = () => {
 			</div>
 		</div>
 	</div>
-	<div className="py-6 text-sm text-center dark:text-gray-50 font-medium">© 2022 All rights reserved, sabikonnahar.sr@gmailcom</div>
+	<div className="py-6 text-sm text-center dark:text-gray-900 font-medium">© 2022 All rights reserved, sabikonnahar.sr@gmailcom</div>
 </footer>
-</div>
-        </div>
     </div>
+</div>
   );
 };
 
